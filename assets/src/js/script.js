af52ff8c1a91d6aa3c264761efdf8f0c1d9d21b7
Mufida Salma <46271228+upida@users.noreply.github.com>
2021-03-12 01:16:06 +0700

    new version
    auto open

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -5,9 +5,14 @@ var article = "";
 fetch(url)
 .then((resp) => resp.json())
 .then(function(data) {
-    data.forEach(function(i) {
+    data.forEach(function(i,key) {
         let item = '<div class="article-item">';
-        let title = '<div class="article-title">'+i.title+'</div>';
+        let title = '';
+        if(key == 1){
+            title = '<div class="article-title show">'+i.title+'</div>';
+        }else{
+            title = '<div class="article-title">'+i.title+'</div>';
+        }
         let body = '<div class="article-body"><div class="article-date">'+i.date+'</div>';
         let text = '<div class="article-text">'+i.content+'</div></div></div>';
         article += item+title+body+text;
