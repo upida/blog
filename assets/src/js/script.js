b08082f955ab9c169ab86e8f04ad1760aa072188
upida <salmamufida34@gmail.com>
2021-08-15 00:58:14 +0700

    delete console log

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -39,8 +39,6 @@ window.onload = function() {
             var root = location.origin;
             var pathparts = location.pathname.split('/');
             var url = root+'/'+pathparts[1].trim('/')+'/'+pathparts[2].trim('/');
-
-            console.log(url);
             
             this.lastScrollPosition = window.pageYOffset;
             window.addEventListener('scroll', this.onScroll);
